723c479e7f9909c6c4944cd2519bffaeae02a095
Ivan Soriano <ivan.soriano@live.com.ar>
2022-09-08 21:28:10 -0300

    Add SIGTERM handler for server

--- server/common/server.py
+++ server/common/server.py
@@ -1,6 +1,7 @@
 import socket
 import logging
-
+import signal
+import sys
 
 class Server:
     def __init__(self, port, listen_backlog):
@@ -8,26 +9,32 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._client_socket = None
+        signal.signal(signal.SIGTERM, self.__sigterm_handler)
 
     def run(self):
         """
         Dummy Server loop
-
         Server that accept a new connections and establishes a
         communication with a client. After client with communucation
         finishes, servers starts to accept new connections again
         """
 
-        # TODO: Modify this program to handle signal to graceful shutdown
-        # the server
         while True:
-            client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
+            self._client_socket = self.__accept_new_connection()
+            self.__handle_client_connection(self._client_socket)
+
+    def __sigterm_handler(self, *args):
+            logging.info("SIGTERM received. Gracefully exiting")
+            logging.info("Closing server socket {}".format(self._server_socket))
+            self._server_socket.close()
+            logging.info("Closing client socket {}".format(self._client_socket))
+            self._client_socket.close()
+            sys.exit(0)
 
     def __handle_client_connection(self, client_sock):
         """
         Read message from a specific client socket and closes the socket
-
         If a problem arises in the communication with the client, the
         client socket will also be closed
         """
@@ -45,7 +52,6 @@ class Server:
     def __accept_new_connection(self):
         """
         Accept new connections
-
         Function blocks until a connection to a client is made.
         Then connection created is printed and returned
         """
